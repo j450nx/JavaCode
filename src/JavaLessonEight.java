f50ed24d67d4f27c2fd2ded1499c983b63647ed9
j450nx <j450nx@gmail.com>
2018-03-19 23:43:25 -0400

    added lesson eight and MonsterTwo

---
+++ src/JavaLessonEight.java
@@ -0,0 +1,10 @@
+public class JavaLessonEight {
+	/*Basic class definition
+	 * public means this class can be used by other classes
+	 * Class names should begin with a capital letter
+	 * A file can't contain two public classes. It can contain classes that are not public
+	 * If you place class files in the same folder the java compiler will find them
+	 */
+	
+	// The goal of this tutorial is to make a game like this
+}
